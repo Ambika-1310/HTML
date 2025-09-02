ca804c1ce8889863a77ba78a5cb1a2bc1a16c802
Ambika k <ambikak9648@gmail.com>
2025-09-02 07:10:29 +0530

    Add files via upload

---
+++ html/icca.js
@@ -0,0 +1,16 @@
+// const li = ['abhi',50,true]
+// li[0] = 'abhishake'
+
+// for(let i=0;i<3;i++)
+// {
+//     document.writeln(li[i])
+// }
+// li=5
+// for(let i=0;i<3;i++)
+// {
+//     document.writeln(li[i])
+// }
+
+let a="icca"
+let b ="dvg"
+document.writeln(${a}is located in '${b}' )
